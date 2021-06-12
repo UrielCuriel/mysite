da1027f2adec52990bc57bb723f7aa4c7ee2dcf3
UrielCuriel <urielcuriel@outlook.com>
2021-06-11 21:48:23 -0500

    fix: contentful import

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,7 +1,7 @@
-import { createClient } from 'contentful'
+import contentful from 'contentful'
 import { config } from '$lib/config'
 
-export const client = createClient({
+export const client = contentful.createClient({
 	space: config.CONTENTFUL_SPACE,
 	environment: config.CONTENTFUL_ENVIRONMENT,
 	accessToken: config.CONTENTFUL_ACCESS_TOKEN
