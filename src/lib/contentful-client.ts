95d787e6dedadfc71d09804fbfb466f1513edf97
Uriel Curiel <urielcuriel@outlook.com>
2021-07-21 17:08:37 -0500

    fix del fix

--- src/lib/contentful-client.ts
+++ src/lib/contentful-client.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { config } from '$lib/config'
-import * as contentful from 'contentful'
-export const client = contentful.createClient({
+import {createClient} from 'contentful/dist/contentful.node'
+export const client = createClient({
 	space: config.CONTENTFUL_SPACE,
 	environment: config.CONTENTFUL_ENVIRONMENT,
 	accessToken: config.CONTENTFUL_ACCESS_TOKEN
